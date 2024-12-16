cb8d0a6b90cd65798f2a89a80fc478e7188cd54b
Mekasu <mekasurenae@gmail.com>
2024-12-15 21:25:10 -0600

    sseparated out contact menu

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,24 +1,36 @@
-
-
 class Helpers:
     def __init__(self):
         pass
 
     def display_greeting(self, printer, colors):
         text = [
+            "~"*50,
+            "\tWelcome To Calculator - Version 0.1.0",
+            "~"*50
+        ]
+
+        return printer.typewriter(
+            content = text,
+            color = colors.BLUE,
+            italic = True,
+        )
+
+    def display_contact(self, printer, colors):
+        printer.reset_console()
+
+        text = [
+            "~"*50,
             "Welcome To Calculator - Version 0.1.0",
-            "\n",
             "Author: mekasu0124",
             "GitHub: https://github.com/mekasu0124/Calculator",
             "Contact:",
             "\temail: mekasurenae@gmail.com",
-            "\temail subject: RE: Calculator // <insert email title>"
+            "\temail subject: RE: Calculator // <insert email title>",
+            "~"*50
         ]
 
-        return printer.write(text, colors.CYAN)
-
-        
-
-    def display_menu(self):
-        pass
-
+        return printer.typewriter(
+            content = text,
+            color = colors.CYAN,
+            italic = True,
+        )
