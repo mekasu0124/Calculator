d981241bf61b82564b3f2e16a5af601204b492b6
Mekasu <mekasurenae@gmail.com>
2024-12-16 16:10:16 -0600

    updating

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,36 +1,61 @@
 class Helpers:
-    def __init__(self):
-        pass
+    def __init__(self, printer, colors):
+        self.printer = printer
+        self.colors = colors
 
-    def display_greeting(self, printer, colors):
+    def display_greeting(self):
         text = [
-            "~"*50,
-            "\tWelcome To Calculator - Version 0.1.0",
-            "~"*50
+            "~"*45,
+            "    Welcome To Calculator - Version 0.1.0",
+            "~"*45
         ]
 
-        return printer.typewriter(
+        return self.printer.typewriter(
             content = text,
-            color = colors.BLUE,
+            color = self.colors.BLUE,
             italic = True,
         )
 
-    def display_contact(self, printer, colors):
-        printer.reset_console()
+    def display_contact(self):
+        self.printer.reset_console()
 
         text = [
-            "~"*50,
+            "~"*75,
             "Welcome To Calculator - Version 0.1.0",
             "Author: mekasu0124",
             "GitHub: https://github.com/mekasu0124/Calculator",
             "Contact:",
             "\temail: mekasurenae@gmail.com",
             "\temail subject: RE: Calculator // <insert email title>",
-            "~"*50
+            "Issues:",
+            "\tgithub: https://github.com/mekasu0124/Calculator/issues",
+            "Releases:",
+            "\tgithub: https://github.com/mekasu0124/Calculator/releases/latest",
+            "~"*75
         ]
 
-        return printer.typewriter(
+        return self.printer.typewriter(
             content = text,
-            color = colors.CYAN,
+            color = self.colors.CYAN,
             italic = True,
         )
+    
+    def get_user_input(self, prompt: str, valid_inputs: list):
+        choice = input(prompt)
+
+        while not choice in valid_inputs:
+            self.printer.typewriter(
+                prompt = f"\nInvalid Input. Input Must Be: {', '.join(valid_inputs)}",
+                color = self.colors.BLACK,
+                background = self.colors.REDB,
+                bold = True,
+                italic = True,
+                underline = True
+            )
+
+            choice = input(f"\n{prompt}")
+
+            if self.validators.is_number(choice):
+                return choice
+        
+        return choice
