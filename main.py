fc17c2e902971bcf3217b7d5d5ada043f84bebff
Mekasu <mekasurenae@gmail.com>
2024-12-15 21:24:36 -0600

    fixed spacing

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from src.utils.helpers import Helpers
 from src.utils.printer import Printer, ConsoleColors
+from src.utils.menu_generator import MenuGenerator
+
+from src.app.standard.app import StandardCalculator
 
 
 class Main:
     """
     ? The Main class is what controls the applications
-    ? life cycle, and which programs run based off user
+    ? life cycle, and which program runs based off user
     ? input.
     """
-    def __init__(self, running, helpers, printer, console_colors):
+    def __init__(self, running, helpers, printer, console_colors, menu_gen, standard):
         """
         ? The main class is initialized with 4 properties:
 
@@ -23,6 +26,9 @@ class Main:
         self.helpers = helpers
         self.printer = printer
         self.colors = console_colors
+        self.menu_gen = menu_gen
+
+        self.standard_calc = standard
 
     def start(self):
         """
@@ -36,8 +42,24 @@ class Main:
 
         self.helpers.display_greeting(self.printer, self.colors)
 
-        self.running = False
-        return self.running
+        menu_choice = self.menu_gen.display_main_menu(self.printer, self.colors)
+
+        if menu_choice == "1":
+            self.standard_calc.start()
+
+        elif menu_choice == "2":
+            print("You Chose Scientific")
+
+        elif menu_choice.lower() == "c":
+            self.helpers.display_contact(self.printer, self.colors)
+            self.start()
+
+        elif menu_choice.lower() == "q":
+            print("You Chose Exit")
+            self.running = False
+            return self.running
+        else:
+            print("Unknown Input")
 
 if __name__ == '__main__':
     running = True
@@ -45,8 +67,11 @@ if __name__ == '__main__':
     helpers = Helpers()
     printer = Printer()
     console_colors = ConsoleColors
+    menu_gen = MenuGenerator()
 
-    main = Main(running, helpers, printer, console_colors)
+    standard = StandardCalculator(helpers, printer, console_colors, menu_gen)
+
+    main = Main(running, helpers, printer, console_colors, menu_gen, standard)
 
     while running:
         """
