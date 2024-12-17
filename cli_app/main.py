121512217916cb6eb438ac9c341117edba49d837
Mekasu <mekasurenae@gmail.com>
2024-12-17 17:14:39 -0600

    updated command names

--- cli_app/main.py
+++ cli_app/main.py
@@ -1,17 +1,23 @@
-from cli_app.commands.hello import hello_command
-from cli_app.commands.calculate import calculate_command
+from cli_app.commands.general.hello import hello_command
+from cli_app.commands.standard.calculate import standard
+from cli_app.commands.scientific.calculate import scientific
 
 import click
 
 
 @click.group()
 def cli():
-    """Simple Python CLI Application"""
+    """
+    A Simple Python CLI Calculator Application
+    
+    Use the commands below to interact with the application.
+    """
     pass
 
 
 cli.add_command(hello_command)
-cli.add_command(calculate_command)
+cli.add_command(standard)
+cli.add_command(scientific)
 
 
 if __name__ == '__main__':
