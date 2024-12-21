7abfb13e2da38ae9582980bf3e0b95a292b20719
Mekasu <mekasurenae@gmail.com>
2024-12-20 21:33:34 -0600

    started menu options class

---
+++ src/utils/menus.py
@@ -0,0 +1,20 @@
+class Menus:
+    main: list = [
+        "1. Standard Calculator\n",
+        "2. Scientific Calculator\n",
+        "8. Previous Calculations\n",
+        "9. Exit Program\n"
+    ]
+    
+    standard: list = [
+        "1. Addition\n",
+        "2. Subtraction\n",
+        "3. Multiplication\n",
+        "4. Division\n",
+        "5. Square Root\n",
+        "6. Percent\n"
+    ]
+
+    scientific: list = [
+        "Not built yet dumbass\n"
+    ]
