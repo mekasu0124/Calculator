acff1a132c583cdceadf0599ced8b19a0e13fc84
Mekasu <mekasurenae@gmail.com>
2024-12-21 20:59:40 -0600

    removed standard menu options

--- src/utils/menus.py
+++ src/utils/menus.py
@@ -5,15 +5,6 @@ class Menus:
         "8. Previous Calculations\n",
         "9. Exit Program\n"
     ]
-    
-    standard: list = [
-        "1. Addition\n",
-        "2. Subtraction\n",
-        "3. Multiplication\n",
-        "4. Division\n",
-        "5. Square Root\n",
-        "6. Percent\n"
-    ]
 
     scientific: list = [
         "Not built yet dumbass\n"
